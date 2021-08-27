d3445ef5359d7117267d777ba0ed33f2bd858cd7
Kwd10104oj3 <hidetoshi.kawada@gmail.com>
2021-08-27 11:50:05 +0000

    Part2 Chapter20 DashBoard(9)

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,9 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
  Route::resource('categories', 'Dashboard\CategoryController')->middleware('auth:admins');
  Route::resource('products', 'Dashboard\ProductController')->middleware('auth:admins');
  Route::resource('users', 'Dashboard\UserController')->middleware('auth:admins');
+ Route::get('orders', 'Dashboard\OrderController@index')->middleware('auth:admins');
+ Route::get('products/import/csv', 'Dashboard\ProductController@import')->middleware('auth:admins');
+ Route::post('products/import/csv', 'Dashboard\ProductController@import_csv')->middleware('auth:admins');
 });
 
 URL::forceScheme('https');
